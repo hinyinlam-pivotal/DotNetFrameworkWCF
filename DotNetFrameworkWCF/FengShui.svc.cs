b01de07461cc0b6c0e02894406fd9c03b3b44e43
Hin Lam <hlam@pivotal.io>
2018-10-04 21:11:58 -0700

    First version of WCF service in .NET framework 4.5

---
+++ DotNetFrameworkWCF/FengShui.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DotNetFrameworkWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class FengShuiService : IFengShui
+    {
+
+        FengShuiDataType IFengShui.GetFengShuiByDate(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        PalmistryDataType ReadPalm(Image palmImage)
+        {
+            return null;
+        }
+
+        PalmistryDataType IFengShui.ReadPalm(Image palmImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DotNetFrameworkWCF/IFengShui.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DotNetFrameworkWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IFengShui
+    //What is Feng Shui? 
+    // https://en.wikipedia.org/wiki/Feng_shui
+    //https://ko.wikipedia.org/wiki/풍수
+    {
+        [OperationContract]
+        FengShuiDataType GetFengShuiByDate(DateTime dateTime);
+
+        [OperationContract]
+        PalmistryDataType ReadPalm(Image palmImage);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class FengShuiDataType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class PalmistryDataType
+    {
+        [DataMember]
+        public String palmSummary;
+
+        [DataMember]
+        public Image palmImage
+        {
+            get { return palmImage; }
+            set { palmImage = value; }
+        }
+    }
+}
